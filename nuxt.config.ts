f06662fdce1c7f830b9cc12858656793976244c9
seifeldin15 <seifeldin02@outlook.com>
2025-03-25 16:11:44 +0200

    Date update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint'],
   eslint: { config: { stylistic: true } },
+
   sourcemap: {
     server: true,
     client: true,
   },
+
+  compatibilityDate: '2025-03-25',
 })
